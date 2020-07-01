75108436a0d59682a9ea6238a45f33cfdd14772f
Martin Bagnall <mbagnall@waratek.com>
2020-07-01 17:53:11 +0100

    Explicity include base_component

--- lib/java_buildpack/framework/waratek_agent.rb
+++ lib/java_buildpack/framework/waratek_agent.rb
@@ -15,6 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+require 'java_buildpack/component/base_component'
 require 'java_buildpack/component/versioned_dependency_component'
 require 'java_buildpack/framework'
 
